6425eef5396051944d115c4fae3e6b7ac119cbf2
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-11-11 14:06:19 -0800

    first

---
+++ wiki/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from wiki.views import PageListView, PageDetailView
+
+
+urlpatterns = [
+    path('', PageListView.as_view(), name='wiki-list-page'),
+    path('<str:slug>/', PageDetailView.as_view(), name='wiki-details-page'),
+]
---
+++ wiki/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from wiki.models import Page
+
+
+class PageListView(ListView):
+    """ Renders a list of all Pages. """
+    model = Page
+
+    def get(self, request):
+        """ GET a list of Pages. """
+        pages = self.get_queryset().all()
+        return render(request, 'list.html', {
+          'pages': pages
+        })
+
+class PageDetailView(DetailView):
+    """ Renders a specific page based on it's slug."""
+    model = Page
+
+    def get(self, request, slug):
+        """ Returns a specific wiki page by slug. """
+        page = self.get_queryset().get(slug__iexact=slug)
+        return render(request, 'page.html', {
+          'page': page
+        })
